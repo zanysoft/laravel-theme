bf0c8449975524a532dfd88aeeee6cc98c58f173
Mubashar Ahmad <mubashar.ahmad@qrgsolutions.com>
2022-08-03 17:49:21 +0500

    no message

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,6 +1,5 @@
 <?php
 if (!function_exists('themes_path')) {
-
     function themes_path($filename = null)
     {
         return app()->make('zanysoft.themes')->themes_path($filename);
@@ -8,15 +7,22 @@ if (!function_exists('themes_path')) {
 }
 
 if (!function_exists('theme_path')) {
-
     function theme_path($path = null, $absolute = true)
     {
         return app()->make('zanysoft.themes')->asset_path($path, $absolute);
     }
 }
 
-if (!function_exists('theme_url')) {
+if (!function_exists('theme_name')) {
+    function theme_name()
+    {
+        $name = app()->make('zanysoft.themes')->get();
 
+        return trim($name);
+    }
+}
+
+if (!function_exists('theme_url')) {
     function theme_url($url, $absolute = true)
     {
         return app()->make('zanysoft.themes')->url($url, $absolute);
